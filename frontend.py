f54a2b054b44ca1182e066967419169ac60fa8c6
Kevin Chang <kevinchang14159@gmail.com>
2017-06-05 19:19:39 -0700

    Fixed some styling

--- frontend.py
+++ frontend.py
@@ -3,9 +3,16 @@ import backend
 
 def add_command():
     """Insert entry via button."""
-    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    backend.insert(title_text.get(),
+                    author_text.get(),
+                    year_text.get(), 
+                    isbn_text.get())
     listing.delete(0, END)
-    listing.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+    listing.insert(END, 
+                    (title_text.get(), 
+                    author_text.get(), 
+                    year_text.get(), 
+                    isbn_text.get()))
 
 def view_command():
     """View entries via button."""
@@ -15,7 +22,11 @@ def view_command():
 
 def update_command():
     """Update entry via button."""
-    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    backend.update(selected_tuple[0], 
+                    title_text.get(), 
+                    author_text.get(), 
+                    year_text.get(), 
+                    isbn_text.get())
 
 def delete_command():
     """Delete entry via button."""
@@ -24,7 +35,10 @@ def delete_command():
 def search_command():
     """Search entry via button."""
     listing.delete(0, END)
-    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+    for row in backend.search(title_text.get(), 
+                                author_text.get(), 
+                                year_text.get(), 
+                                isbn_text.get()):
         listing.insert(END, row)
 
 def get_selected_row(event):
@@ -94,22 +108,40 @@ scroller.configure(command = listing.yview)
 listing.bind('<<ListboxSelect>>', get_selected_row)
 
 # Buttons for various operations on data.
-button1 = Button(window, text = "View All", width = 12, command = view_command)
+button1 = Button(window, 
+                text = "View All", 
+                width = 12, 
+                command = view_command)
 button1.grid(row = 2, column = 3)
 
-button2 = Button(window, text = "Search Entry", width = 12, command = search_command)
+button2 = Button(window, 
+                text = "Search Entry", 
+                width = 12, 
+                command = search_command)
 button2.grid(row = 3, column = 3)
 
-button3 = Button(window, text = "Add Entry", width = 12, command = add_command)
+button3 = Button(window, 
+                text = "Add Entry", 
+                width = 12, 
+                command = add_command)
 button3.grid(row = 4, column = 3)
 
-button4 = Button(window, text = "Update Selected", width = 12, command = update_command)
+button4 = Button(window, 
+                text = "Update Selected", 
+                width = 12, 
+                command = update_command)
 button4.grid(row = 5, column = 3)
 
-button5 = Button(window, text = "Delete Selected", width = 12, command = delete_command)
+button5 = Button(window, 
+                text = "Delete Selected", 
+                width = 12, 
+                command = delete_command)
 button5.grid(row = 6, column = 3)
 
-button6 = Button(window, text = "Close", width = 12, command = window.destroy)
+button6 = Button(window, 
+                text = "Close", 
+                width = 12, 
+                command = window.destroy)
 button6.grid(row = 7, column = 3)
 
 # Keep window open until closed.
